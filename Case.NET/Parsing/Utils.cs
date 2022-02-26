4a6227135fa4f3bd45a0742b83469eb19725efc2
2CHEVSKII <prsroman3@gmail.com>
2022-02-26 20:05:02 +0300

    Add Utils (internal)

---
+++ Case.NET/Parsing/Utils.cs
@@ -0,0 +1,16 @@
+﻿namespace Case.NET.Parsing
+{
+    internal static class Utils
+    {
+        internal static bool Contains(char[] array, char c)
+        {
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                if (array[i] == c)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
