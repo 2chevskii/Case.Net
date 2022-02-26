f71688dfdd26988fea8b378f7b34e6b5e12813da
2CHEVSKII <prsroman3@gmail.com>
2022-02-27 00:58:12 +0300

    rewrite Parser

--- Case.NET/Parsing/Parser.cs
+++ Case.NET/Parsing/Parser.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using Case.NET.Parsing.Filters;
 using Case.NET.Parsing.Tokens;
 using Case.NET.Parsing.WordSplitting;
 
@@ -8,68 +10,111 @@ namespace Case.NET.Parsing
 {
     public class Parser : IParser
     {
-        public static readonly IParser UniversalParser = new Parser(
-            new IWordSplitter[] {
-                new LineEndWordSplitter(),
-                new DashWordSplitter(),
-                new UnderscoreWordSplitter(),
-                new SpaceWordSplitter()
-            }
-        );
+        private static readonly ICharFilter[] EmptyCharFilterArray = Array.Empty<ICharFilter>();
+
         protected readonly IWordSplitter[] wordSplitters;
+        protected readonly ICharFilter[]   charFilters;
+        protected readonly int[]           splitIndexArray; // those two arrays are used for memoizing values of the current pass
+        protected readonly bool[]          skipCharsArray;  // therefore - this Parser impl is not thread-safe
 
         public IReadOnlyCollection<IWordSplitter> WordSplitters => wordSplitters;
+        public IReadOnlyCollection<ICharFilter> CharFilters => charFilters;
 
-        protected Parser(IEnumerable<IWordSplitter> wordSplitters)
+        public Parser(params IWordSplitter[] wordSplitters) : this(
+            (IEnumerable<IWordSplitter>) wordSplitters
+        ) { }
+
+        public Parser(IEnumerable<IWordSplitter> wordSplitters) : this(wordSplitters, null) { }
+
+        public Parser(
+            IEnumerable<IWordSplitter> wordSplitters,
+            IEnumerable<ICharFilter> charFilters
+        )
         {
-            this.wordSplitters = wordSplitters.ToArray();
+            this.wordSplitters = wordSplitters?.ToArray() ??
+                                 throw new ArgumentNullException(nameof(wordSplitters));
+
+            if (this.wordSplitters.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Word splitters array cannot be empty",
+                    nameof(wordSplitters)
+                );
+            }
+
+            this.charFilters = charFilters?.ToArray() ?? EmptyCharFilterArray;
+            splitIndexArray = new int[this.wordSplitters.Length];
+            skipCharsArray = new bool[this.wordSplitters.Length];
+            Utils.FillArray(splitIndexArray, -1);
         }
 
-        public virtual ICollection<IToken> Parse(string value, bool includeSplitTokens)
+        public virtual ICollection<IToken> Parse(
+            string value,
+            bool includeSplitTokens,
+            bool returnSourceIfNoMatches
+        )
         {
             List<IToken> tokens = new List<IToken>();
-            int lastIndex = 0;
 
-            for (var i = 0; i < value.Length; i++)
+            int startAt = 0;
+
+            while (startAt < value.Length)
             {
-                int bufferedChars = i - lastIndex;
-
-                for (var j = 0; j < wordSplitters.Length; j++)
+                for (int i = 0; i < charFilters.Length; i++)
                 {
-                    var splitter = wordSplitters[j];
+                    ICharFilter filter = charFilters[i];
 
-                    bool splitted = splitter.CanSplit(
-                        value,
-                        i,
-                        bufferedChars,
-                        out bool skip,
-                        out SplitToken splitToken
-                    );
+                    while (startAt < value.Length && filter.ShouldSkip(value, startAt))
+                        startAt++;
+                }
 
-                    if (splitted)
+                if (startAt == value.Length)
+                    break;
+
+                for (int i = 0; i < wordSplitters.Length; i++)
+                {
+                    IWordSplitter splitter = wordSplitters[i];
+
+                    int index = splitter.TryFindSplitIndex(value, startAt, out bool skip);
+
+                    splitIndexArray[i] = index;
+                    skipCharsArray[i] = skip;
+                }
+
+                int minIndex = -1;
+                bool doSkip = false;
+
+                for (int i = 0; i < splitIndexArray.Length; i++)
+                {
+                    int index = splitIndexArray[i];
+
+                    if (index != -1 && index < minIndex)
                     {
-                        string val = value.Substring(lastIndex, i - lastIndex);
-                        tokens.Add(new WordToken(lastIndex, val));
-
-                        if (includeSplitTokens)
-                        {
-                            tokens.Add(splitToken);
-                        }
-
-                        lastIndex = i + 1;
-                    }
-
-                    if (skip)
-                    {
-                        if (includeSplitTokens)
-                        {
-                            tokens.Add(splitToken);
-                        }
-
-                        lastIndex = i + 1;
-                        break;
+                        minIndex = index;
+                        doSkip = skipCharsArray[i];
                     }
                 }
+
+                int length = (minIndex != -1 ? minIndex : value.Length) - startAt;
+
+                string substring = value.Substring(startAt, length);
+
+                WordToken word = new WordToken(startAt, substring);
+
+                if (minIndex != -1)
+                {
+                    tokens.Add(word);
+                    startAt = doSkip ? minIndex + 1 : minIndex;
+                }
+                else
+                {
+                    startAt = value.Length;
+                }
+            }
+
+            if (tokens.Count == 0 && returnSourceIfNoMatches)
+            {
+                tokens.Add(new WordToken(0, value));
             }
 
             return tokens;
