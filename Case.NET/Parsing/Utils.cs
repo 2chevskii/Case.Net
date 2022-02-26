3bf44b4e891d0016eddea59cd5e8116fe2ad0495
2CHEVSKII <prsroman3@gmail.com>
2022-02-27 01:48:08 +0300

    upd Utils

--- Case.NET/Parsing/Utils.cs
+++ Case.NET/Parsing/Utils.cs
@@ -1,16 +1,26 @@
-﻿namespace Case.NET.Parsing
+using System.Collections.Generic;
+
+namespace Case.NET.Parsing
 {
     internal static class Utils
     {
-        internal static bool Contains(char[] array, char c)
+        internal static bool Contains(IReadOnlyList<char> array, char c)
         {
-            for (var i = array.Length - 1; i >= 0; i--)
+            for (var i = array.Count - 1; i >= 0; i--)
             {
                 if (array[i] == c)
                     return true;
             }
 
             return false;
+        }
+
+        internal static void FillArray<T>(T[] array, T value) where T : struct
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
         }
     }
 }
