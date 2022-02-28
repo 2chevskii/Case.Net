a144488739c6056fe3f8a4cd5371f40bc6bcb8fd
2CHEVSKII <prsroman3@gmail.com>
2022-02-28 13:30:46 +0300

    Update StringExtensions

--- Case.NET.Extensions/StringExtensions.cs
+++ Case.NET.Extensions/StringExtensions.cs
@@ -1,37 +1,20 @@
-using System;
+using static Case.NET.CaseConverter;
 
 namespace Case.NET.Extensions
 {
     public static class StringExtensions
     {
-        public static CasedString ToCamelCase(this string self)
-        {
-            return CaseConverter.CamelCase.ConvertCase(self);
-        }
+        public static CasedString ToCamelCase(this string self) => CamelCase.ConvertCase(self);
 
-        public static CasedString ToPascalCase(this string self)
-        {
-            return CaseConverter.PascalCase.ConvertCase(self);
-        }
+        public static CasedString ToPascalCase(this string self) => PascalCase.ConvertCase(self);
 
-        public static CasedString ToSnakeCase(this string self)
-        {
-            throw new NotImplementedException();
-        }
+        public static CasedString ToSnakeCase(this string self) => SnakeCase.ConvertCase(self);
 
-        public static CasedString ToAllUpperCase(this string self)
-        {
-            throw new NotImplementedException();
-        }
+        public static CasedString ToConstantCase(this string self) =>
+            ConstantCase.ConvertCase(self);
 
-        public static CasedString ToKebabCase(this string self)
-        {
-            throw new NotImplementedException();
-        }
+        public static CasedString ToKebabCase(this string self) => KebabCase.ConvertCase(self);
 
-        public static CasedString ToTrainCase(this string self)
-        {
-            throw new NotImplementedException();
-        }
+        public static CasedString ToTrainCase(this string self) => TrainCase.ConvertCase(self);
     }
 }
