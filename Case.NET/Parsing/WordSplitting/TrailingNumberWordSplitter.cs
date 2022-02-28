044cb27319d3ee6fcd161ba8195f349209184ee5
2CHEVSKII <prsroman3@gmail.com>
2022-02-28 15:16:28 +0300

    Fix TrailingNumberWordSplitter not skipping leading digits

--- Case.NET/Parsing/WordSplitting/TrailingNumberWordSplitter.cs
+++ Case.NET/Parsing/WordSplitting/TrailingNumberWordSplitter.cs
@@ -16,19 +16,26 @@ namespace Case.NET.Parsing.WordSplitting
         {
             skipIndexChar = false;
 
+            // skip leading digits
+            while (startAt < value.Length && char.IsDigit(value[startAt]))
+            {
+                startAt++;
+            }
+
             for (int i = startAt + 1; i < value.Length; i++)
             {
                 char c = value[i];
+
                 char cPrev = value[i - 1];
 
-                if (!char.IsDigit(cPrev) || !char.IsLetter(c))
+                if (!char.IsDigit(cPrev) || char.IsDigit(c))
                 {
                     continue;
                 }
 
                 return i;
             }
-
+            
             return -1;
         }
     }
