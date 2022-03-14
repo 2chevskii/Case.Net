cee480ac44d2e1d9a4abd5ac6f7d402e7e038d3f
2CHEVSKII <prsroman3@gmail.com>
2022-03-14 22:24:13 +0300

    Add KnownCaseKind, no use for now

---
+++ Case.NET/KnownCaseKind.cs
@@ -0,0 +1,23 @@
+namespace Case.NET
+{
+    public enum KnownCaseKind
+    {
+        PASCAL,
+        CAMEL,
+        SNAKE,
+        CONSTANT,
+        KEBAB,
+        TRAIN,
+        DOT,
+        NAMESPACE,
+        NO,
+        PATH_FORWARD,
+        PATH_BACKWARD,
+        SENTENCE,
+        FENCE,
+        SPONGE,
+        SPONGE_REVERSE,
+        TITLE,
+        UNKNOWN
+    }
+}
