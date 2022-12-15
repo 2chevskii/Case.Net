7949da2dd3e4bf38633adcfe22b6c81670a64082
2CHEVSKII <prsroman3@gmail.com>
2022-12-15 03:12:39 +0300

    Add supporting projects to extend System.Text.Json & Newtonsoft.Json

---
+++ Case.Net/Common/Conventions/MixedNamingConvention.cs
@@ -0,0 +1,11 @@
+﻿namespace Case.Net.Common.Conventions;
+
+public class MixedNamingConvention : NamingConvention
+{
+    public override string Name => "Mixed";
+
+    public override CasedString Convert(CasedString source)
+    {
+        return source;
+    }
+}
