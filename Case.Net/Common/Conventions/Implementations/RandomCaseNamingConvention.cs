d9aca47e734fba30796c90d64f7c348e63a68c43
2CHEVSKII <prsroman3@gmail.com>
2022-12-21 17:59:59 +0300

    start impl of random naming convention

---
+++ Case.Net/Common/Conventions/Implementations/RandomCaseNamingConvention.cs
@@ -0,0 +1,49 @@
+﻿using Case.Net.Common.Entities;
+
+namespace Case.Net.Common.Conventions;
+
+public class RandomCaseNamingConvention : NamingConvention
+{
+
+    public RandomCaseNamingConvention(string name) : base( name ) { }
+
+    public override bool TryConvert(CasedString input, out CasedString output)
+    {
+        var words    = input.Words;
+        var wordsOut = new string[words.Count];
+        for ( var i = 0; i < words.Count; i++ )
+        {
+            var word   = words[i];
+            var buffer = new char[word.Length];
+            for ( var j = 0; j < word.Length; j++ )
+            {
+                buffer[j] = IsUpper()
+                ? char.ToUpperInvariant( word[j] )
+                : char.ToLowerInvariant( word[j] );
+            }
+
+            wordsOut[i] = new string( buffer );
+        }
+
+
+    }
+
+    static bool IsUpper()
+    {
+        return Random.Shared.Next( 2 ) > 0;
+    }
+
+    public override bool TryParse(ReadOnlySpan<char> input, out CasedString output)
+    {
+        Parse( input ); // throws
+        output = CasedString.Empty;
+        return false;
+    }
+
+    public override CasedString Parse(ReadOnlySpan<char> input)
+    {
+        throw new InvalidOperationException(
+            $"{nameof( RandomCaseNamingConvention )} is unable to parse"
+        );
+    }
+}
